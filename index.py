90edba17ed48582e8484e1a7c121b64de73640e5
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-11 10:40:02 -0400

    mvp commit for illegal names in python

---
+++ index.py
@@ -0,0 +1,44 @@
+# Type the illegal character in this variable name:
+# 2nd_address
+2
+
+# Type the illegal character in this variable name:
+# fourth-Player
+# -
+
+# Rewrite this variable name using the recommended style: customerlastname
+custom_last_name = "J" 
+
+# Rewrite this variable name using the recommended style:
+# totalaftertax
+total_after_tax = 3
+
+# Make up a variable name that includes a number.
+cost = 100
+
+# Assign a number to a variable. Make up the number and the variable name.
+name = "Justin"
+
+# Assign one variable to another. Make up the variable names.
+time = 10
+time = time
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+varry_one = 12
+varry_two = 12
+varry_three = varry_one / varry_two
+print(varry_three)
+
+# Correct the variable naming errors.
+
+# 4h="yes"
+# negative answer = "no"
+# asian_country = "China"
+# african-country = "Kenya"
+# print("ok")
+
+awnswer = "yes"
+negative_awnser = "no"
+asian_country = "China"
+african_country = "Africa"
+print("ok")
